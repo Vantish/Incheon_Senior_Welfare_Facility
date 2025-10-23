e9f494dcefd2013158177d85a1212258414a3bb0
Lucky0626 <kmj115711@gmail.com>
2025-10-23 10:38:50 +0900

    로케이션 수정

--- app_bus_stop_recommendation.py
+++ app_bus_stop_recommendation.py
@@ -14,10 +14,12 @@ from geopy.distance import distance
 # 해당 정보를 바탕으로 계산된 시설의 위치정보(facilities_location)를 입력받은 후
 # 사용자 근처 가장 가까운 정류장 5개와 시설에서 가장 가까운 정류장 5개를 딕셔너리 형태로 반환합니다.
 
-facilities_location = pd.read_csv('./data/인천광역시_노인복지시설_현황')
+facilities_location = pd.read_csv('./data/인천광역시_노인복지시설 현황.csv', encoding='euc-kr')
 bus_stops_df = pd.read_csv('./data/버스정류장.csv')
 
 def bus_stop_recommendation(user_location, facilities_location):
+
+    
     
     
    
@@ -25,5 +27,3 @@ def bus_stop_recommendation(user_location, facilities_location):
     
     return
 
-def bus_stop_recommendation(user_location, selected_facility,stops_df=bus_stops, n=5 ):
-    pass
