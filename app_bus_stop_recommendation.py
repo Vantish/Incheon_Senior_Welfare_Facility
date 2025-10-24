e6ef8adaa87592dc77b1991ee7c8294e0ec8315b
Lucky0626 <kmj115711@gmail.com>
2025-10-24 15:26:35 +0900

    수정사항없음

--- app_bus_stop_recommendation.py
+++ app_bus_stop_recommendation.py
@@ -6,7 +6,6 @@ from sklearn.neighbors import NearestNeighbors
 from geopy.geocoders import Nominatim
 from geopy.distance import distance
 from geopy.distance import geodesic
-
 from app_location import run_location
 
 # 기존에는 버스 정보를 받아서 정류장을 찾는 형태였는데
@@ -18,27 +17,6 @@ from app_location import run_location
 
 bus_stops_df = pd.read_csv('./data/버스정류장.csv')
 
+
 def bus_stop_recommendation(user_location, facilities_location):
-    user_location = run_location()
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
     return
-
-   
-    
-
-    
-  
-
